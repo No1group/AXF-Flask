e8f98f96d8ced0083c8f6ae4b3156ddf8d3e1b13
zhuoya123 <1909006646@qq.com>
2018-03-17 16:26:57 +0800

    超市视图修改

--- App/views/view_market.py
+++ App/views/view_market.py
@@ -1,4 +1,6 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, redirect, url_for
+
+from App.models.models import FoodType, Goods
 
 marketbp = Blueprint("marketbp", __name__)
 
@@ -9,15 +11,16 @@ def init_marketbp(app):
 
 @marketbp.route('/market/')
 def index():
-    return '薛科的ｍａｒｋｅｔ页面'
+    return '薛科的market页面'
 
+@marketbp.route('/markets/')
 def market(request):
-    return redirect(reverse("app:marketWithParams", kwargs={"typeid": "104749", "childcid": '0', "sortrule": "0"}))
+    return redirect(url_for("marketbp.marketWithParams", kwargs={"typeid": "104749", "childcid": '0', "sortrule": "0"}))
 
-
-def marketWithParams(request, typeid, childcid, sortrule):
-    foodtypes = FoodType.objects.all()
-    goodsList = Goods.objects.filter(categoryid=typeid)
+@marketbp.route('/')
+def marketWithParams(typeid, childcid, sortrule):
+    foodtypes = FoodType.query.all()
+    goodsList = Goods.query.filter(categoryid=typeid)
 
     if childcid != "0":
         goodsList = goodsList.filter(childcid=childcid)
@@ -59,20 +62,21 @@ def marketWithParams(request, typeid, childcid, sortrule):
         "typeid": typeid,
         "childcid": childcid,
     }
-    return render(request, 'app/market/market.html', context=data)
+    return render_template('market.html',context =data)
+        # render(request, 'app/market/market.html', context=data)
 
-def notPayList(request):
-    username = request.session.get("username")
+# def notPayList(request):
+#     username = request.session.get("username")
+#
+#     data = getOrders(username,0)
+#
+#     return render(request,'app/market/order/order_list.html',context=data)
 
-    data = getOrders(username,0)
 
-    return render(request,'app/market/order/order_list.html',context=data)
-
-
-def notReceiveList(request):
-
-    username = request.session.get("username")
-
-    data = getOrders(username,1)
-
-    return render(request,'app/market/order/order_list_not_receive.html',context=data)
+# def notReceiveList(request):
+#
+#     username = request.session.get("username")
+#
+#     data = getOrders(username,1)
+#
+#     return render(request,'app/market/order/order_list_not_receive.html',context=data)
